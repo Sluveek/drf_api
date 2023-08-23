e98ec34ae9a2c72944e984224553ed7bf7281cfe
Veaveslav Chirita <you@example.com>
2023-08-23 15:59:51 +0200

    added CommentList and CommentDetail

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('', include('profiles.urls')),
-    path('', include('posts.urls')),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("", include("profiles.urls")),
+    path("", include("posts.urls")),
+    path("", include("comments.urls")),
 ]
